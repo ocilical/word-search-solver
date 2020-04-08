48d3b8518080ed2d58f59d0f00407ea2944538cd
ocilical <ocilical@gmail.com>
2020-04-08 18:26:27 -0400

    half the diagonals work now

--- solver.js
+++ solver.js
@@ -22,6 +22,7 @@ function getCol(arr, col) {
     return column;
 }
 
+//this thing is a mess, watch out
 function solvePuzzle() {
     //array of answers
     let answers = [];
@@ -52,14 +53,58 @@ function solvePuzzle() {
         //check vertical
         for (let i = 0; i < grid[0].length; i++) {
             let col = getCol(grid, i).join("");
-            console.log(col);
             //down
             if (col.includes(word)) {
                 answers.push(new Answer(word, i, col.indexOf(word), "down"));
             }
             //up
             if (reverseString(col).includes(word)) {
-                answers.push(new Answer(word, i, col.length - (reverseString(col).indexOf(word) + 1), "up"))
+                answers.push(new Answer(word, i, col.length - (reverseString(col).indexOf(word) + 1), "up"));
+            }
+        }
+
+        //check diagonals (don't even bother trying to understand this, it's terrible)
+        let width = grid[0].length;
+        let height = grid.length;
+        let numDiag = width + height - 1
+
+        //diag ↗
+        //top half
+        for (let k = 0; k < height; k++) {
+            let i = k;
+            let j = 0;
+            let diag = "";
+            while (i >= 0) {
+                diag += grid[i][j];
+                i--;
+                j++;
+            }
+            //up right
+            if (diag.includes(word)) {
+                answers.push(new Answer(word, diag.indexOf(word), k - diag.indexOf(word), "up right"));
+            }
+            //down left
+            if (reverseString(diag).includes(word)) {
+                answers.push(new Answer(word, k - reverseString(diag).indexOf(word), reverseString(diag).indexOf(word), "down left"));
+            }
+        }
+        //bottom half
+        for (let k = 1; k < width; k++) {
+            let i = height - 1;
+            let j = k;
+            let diag = "";
+            while(i >= 0) {
+                diag += grid[i][j];
+                i--;
+                j++;
+            }
+            //up right
+            if(diag.includes(word)) {
+                answers.push(new Answer(word, k + diag.indexOf(word), (height - 1) - diag.indexOf(word), "up right"));
+            }
+            //down left
+            if(reverseString(diag).includes(word)) {
+                answers.push(new Answer(word, (k-1) + word.length, (height - 1) - (diag.length - reverseString(diag).indexOf(word)) + 1 ,"down left"))
             }
         }
 
@@ -83,7 +128,5 @@ function solvePuzzle() {
     }
 
 }
-
-
 
 solveButton.addEventListener("click", solvePuzzle);
