476fce9b0af0bc81f711c617f8afae0681695f70
ocilical <ocilical@gmail.com>
2020-04-08 19:47:21 -0400

    Complete overhaul of solve method
    The old one was terrible and dumb, this one actually works

--- solver.js
+++ solver.js
@@ -33,78 +33,45 @@ function solvePuzzle() {
         arr[index] = element.split("");
     });
 
+    //grid width and height
+    let width = grid[0].length;
+    let height = grid.length;
+
     //get array of words to look for
     let words = document.getElementById("wordInput").value.split(/[ ,]+/);
 
+    //direction arrays
+    let dirNames = ["up left", "up", "up right", "left", "right", "down left", "down", "down right"];
+    let dirsX = [-1, 0, 1, -1, 1, -1, 0, 1];
+    let dirsY = [-1, -1, -1, 0, 0, 1, 1, 1];
+
+
     for (const word of words) {
-        //check horizontal
         for (let i = 0; i < grid.length; i++) {
-            let row = grid[i].join("");
-            //forward
-            if (row.includes(word)) {
-                answers.push(new Answer(word, row.indexOf(word), i, "right"));
-            }
-            //backwards
-            if (reverseString(row).includes(word)) {
-                answers.push(new Answer(word, row.length - (reverseString(row).indexOf(word) + 1), i, "left"))
-            }
-        }
+            for (let j = 0; j < grid[0].length; j++) {
+                if (word.startsWith(grid[i][j])) {
+                    const len = word.length;
+                    for (let dir = 0; dir < 8; dir++) {
+                        let iDir = i;
+                        let jDir = j;
+                        let pos = 0;
+                        for (pos = 0; pos < len; pos++) {
+                            if (iDir >= height || iDir < 0 || jDir >= width || iDir < 0) {
+                                break;
+                            }
+                            if (word.charAt(pos) !== grid[iDir][jDir]) {
+                                break;
+                            }
+                            iDir += dirsY[dir];
+                            jDir += dirsX[dir];
+                            console.log(word)
+                        }
+                        if(pos === len){
+                            answers.push(new Answer(word, i, j, dirNames[dir]))
+                        }
 
-        //check vertical
-        for (let i = 0; i < grid[0].length; i++) {
-            let col = getCol(grid, i).join("");
-            //down
-            if (col.includes(word)) {
-                answers.push(new Answer(word, i, col.indexOf(word), "down"));
-            }
-            //up
-            if (reverseString(col).includes(word)) {
-                answers.push(new Answer(word, i, col.length - (reverseString(col).indexOf(word) + 1), "up"));
-            }
-        }
-
-        //check diagonals (don't even bother trying to understand this, it's terrible)
-        let width = grid[0].length;
-        let height = grid.length;
-        let numDiag = width + height - 1
-
-        //diag ↗
-        //top half
-        for (let k = 0; k < height; k++) {
-            let i = k;
-            let j = 0;
-            let diag = "";
-            while (i >= 0) {
-                diag += grid[i][j];
-                i--;
-                j++;
-            }
-            //up right
-            if (diag.includes(word)) {
-                answers.push(new Answer(word, diag.indexOf(word), k - diag.indexOf(word), "up right"));
-            }
-            //down left
-            if (reverseString(diag).includes(word)) {
-                answers.push(new Answer(word, k - reverseString(diag).indexOf(word), reverseString(diag).indexOf(word), "down left"));
-            }
-        }
-        //bottom half
-        for (let k = 1; k < width; k++) {
-            let i = height - 1;
-            let j = k;
-            let diag = "";
-            while(i >= 0) {
-                diag += grid[i][j];
-                i--;
-                j++;
-            }
-            //up right
-            if(diag.includes(word)) {
-                answers.push(new Answer(word, k + diag.indexOf(word), (height - 1) - diag.indexOf(word), "up right"));
-            }
-            //down left
-            if(reverseString(diag).includes(word)) {
-                answers.push(new Answer(word, (k-1) + word.length, (height - 1) - (diag.length - reverseString(diag).indexOf(word)) + 1 ,"down left"))
+                    }
+                }
             }
         }
 
@@ -122,8 +89,8 @@ function solvePuzzle() {
     for (const ans of answers) {
         let newRow = table.getElementsByTagName("tbody")[0].insertRow(-1);
         newRow.insertCell(-1).innerHTML = ans.word;
-        newRow.insertCell(-1).innerHTML = ans.x;
-        newRow.insertCell(-1).innerHTML = ans.y;
+        newRow.insertCell(-1).innerHTML = ans.x+1;
+        newRow.insertCell(-1).innerHTML = ans.y+1;
         newRow.insertCell(-1).innerHTML = ans.dir;
     }
 
